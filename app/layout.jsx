e26f373aaea24bdf8c039345c3bea2e519c96490
TreciaKS <tksekgalo@gmail.com>
2022-11-22 17:47:41 +0200

    continue with navbar styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
+"use client" // declare a NextJs13 component as a client component
 import Link from "next/link";
 import "./globals.css";
+import { Menu } from "@headlessui/react";
 
 export default function RootLayout({ children }) {
   return (
@@ -16,27 +18,56 @@ export default function RootLayout({ children }) {
             <Link href="/posts">Posts</Link>
           </nav>
         </header> */}
-        <header className="text-gray-600 body-font">
-          <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <header className="text-gray-600">
+          <div className="container flex flex-col flex-wrap items-center p-1 mx-auto lg:p-0 md:flex-row">
+            <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
               {/* will substitute strapi logo here, first designing navbar */}
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 p-2 text-white bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span className="ml-3 text-xl">Tailblocks</span>
+              <span className="ml-3 text-xl">Strapi</span>
             </a>
-            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-              <Link className='mr-5 hover:text-gray-900' href='/'>Home</Link>
-              <Link className='mr-5 hover:text-gray-900' href="/posts">Posts</Link>
+            <nav className="flex flex-wrap items-center justify-center text-base md:mr-auto md:ml-4 md:py-1 md:pl-4">
+              <Link className='mr-5 hover:text-gray-900' href='/'>Pricing</Link>
+              <Link className='mr-5 hover:text-gray-900' href="/posts">Contact</Link>
             </nav>
-            <div className="flex justify-between">
-            <button className="inline-flex items-center border-2 py-2 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 text-indigo-600 hover:text-black border-indigo-600">Sign Up
+            <div className="flex justify-between w-auto">
+            <button className="inline-flex items-center px-4 py-2 mt-4 text-base text-indigo-600 bg-white border-2 border-indigo-600 rounded focus:outline-none hover:bg-gray-200 md:mt-0 hover:text-black">Sign Up
             </button>
-            <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">En
+            {/* <button className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">En
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
-            </button>
+            </button> */}
+            <div className="relative inline-flex items-stretch bg-white border rounded-md">
+              <Menu>
+                <Menu.Button>🌏 En </Menu.Button>
+                  <div className="absolute right-0 z-10 w-56 mt-4 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg">
+                  <Menu.Items>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${active && "block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"}`}
+                          
+                        >
+                          En
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${active && "block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"}`}
+                          
+                        >
+                          Fr
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                  </div>
+                </Menu>
+            </div>
             </div>
           </div>
         </header>
