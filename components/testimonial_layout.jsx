dd061f54902fa0a61088a90ed78900a6d4001ea0
TreciaKS <tksekgalo@gmail.com>
2022-12-06 13:00:50 +0200

    spelling of className

--- components/testimonial_layout.jsx
+++ components/testimonial_layout.jsx
@@ -4,14 +4,14 @@ import Testimonial from './testimonial_card'
 const testimonial_layout = () => {
   return (
     <>
-    <section class="bg-white">
-        <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-4 lg:px-8">
-            <div class="mx-auto max-w-xl text-center">
-                <h2 class="text-4xl font-bold tracking-tight sm:text-5xl">
+    <section className="bg-white">
+        <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 sm:py-4 lg:px-8">
+            <div className="max-w-xl mx-auto text-center">
+                <h2 className="text-4xl font-bold tracking-tight sm:text-5xl">
                     Read trusted reviews from our customers
                 </h2>
 
-                <p class="text-gring-offset-warm-gray-500 mx-auto mt-4 max-w-lg">
+                <p className="max-w-lg mx-auto mt-4 text-gring-offset-warm-gray-500">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
                     praesentium natus sapiente commodi. Aliquid sunt tempore iste
                     repellendus explicabo dignissimos placeat, autem harum dolore
@@ -22,16 +22,16 @@ const testimonial_layout = () => {
         </div>
         <Testimonial />
         {/* Logo section */}
-            <div class="px-4 py-6 mx-auto max-w-3xl">
-                <div class="grid grid-cols-3 gap-5">
-                    <div class="flex items-center justify-center">
-                        <div class='h-12 w-12 rounded-full bg-gray-400'></div>
+            <div className="max-w-3xl px-4 py-6 mx-auto">
+                <div className="grid grid-cols-3 gap-5">
+                    <div className="flex items-center justify-center">
+                        <div className='w-12 h-12 bg-gray-400 rounded-full'></div>
                     </div>
-                    <div class="flex items-center justify-center">
-                        <div class='h-12 w-12 rounded-full bg-gray-400'></div>
+                    <div className="flex items-center justify-center">
+                        <div className='w-12 h-12 bg-gray-400 rounded-full'></div>
                     </div>
-                    <div class="flex items-center justify-center">
-                        <div class='h-12 w-12 rounded-full bg-gray-400'></div>
+                    <div className="flex items-center justify-center">
+                        <div className='w-12 h-12 bg-gray-400 rounded-full'></div>
                     </div>
                 </div>
             </div>
