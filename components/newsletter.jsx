ff802eba133723e925a07a30728db19e5b02b35b
TreciaKS <tksekgalo@gmail.com>
2022-12-06 12:59:47 +0200

    corrected spelling

--- components/newsletter.jsx
+++ components/newsletter.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 const newsletter = () => {
   return (
     <div>
-        <section class="bg-gray-100">
-            <div class="p-8 md:p-12 lg:px-16 lg:py-10 mt-10">
-                <div class="mx-auto max-w-lg text-center">
-                <h2 class="text-2xl font-bold text-gray-900 md:text-3xl">
+        <section className="bg-gray-100">
+            <div className="p-8 mt-10 md:p-12 lg:px-16 lg:py-10">
+                <div className="max-w-lg mx-auto text-center">
+                <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
                     Subscribed To Our Newsletter
                 </h2>
                 </div>
 
-                <div class="mx-auto mt-8 max-w-xl">
-                <form action="#" class="sm:flex sm:gap-4">
-                    <div class="sm:flex-1">
-                    <label for="email" class="sr-only">Email</label>
+                <div className="max-w-xl mx-auto mt-8">
+                <form action="#" className="sm:flex sm:gap-4">
+                    <div className="sm:flex-1">
+                    <label for="email" className="sr-only">Email</label>
 
                     <input
                         type="email"
                         placeholder="Email address"
-                        class="w-full rounded-md border-indigo-600 bg-white p-3 text-gray-700 shadow-sm transition focus:border-white focus:outline-none focus:ring focus:ring-indigo-500"
+                        className="w-full p-3 text-gray-700 transition bg-white border-indigo-600 rounded-md shadow-sm focus:border-white focus:outline-none focus:ring focus:ring-indigo-500"
                     />
                     </div>
 
                     <button
                     type="submit"
-                    class="group mt-4 flex w-full items-center justify-center rounded-md bg-white px-5 py-3 text-indigo-600 transition focus:outline-none sm:mt-0 sm:w-auto border-2 border-indigo-600"
+                    className="flex items-center justify-center w-full px-5 py-3 mt-4 text-indigo-600 transition bg-white border-2 border-indigo-600 rounded-md group focus:outline-none sm:mt-0 sm:w-auto"
                     >
-                    <span class="text-sm font-medium"> Sign Up </span>
+                    <span className="text-sm font-medium"> Sign Up </span>
                     </button>
                 </form>
                 </div>
